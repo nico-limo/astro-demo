b71aadc9ce6958468a6e69a1c19e706fbcb90d66
nico-limo <nicolas.limonoff@gmail.com>
2024-09-23 12:21:39 -0300

    added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import icon from 'astro-icon'
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: vercel()
 })
